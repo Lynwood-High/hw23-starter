cd81366685f233a26b78c3240f5e427b6cb9fd10
Emma <40247894+si-wang@users.noreply.github.com>
2021-01-14 00:01:53 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+class Main {
+  public static void main(String[] args) {
+
+    System.out.println("\nQuestion 1");
+
+    System.out.println("\nQuestion 2");
+
+    System.out.println("\nQuestion 3");
+
+    System.out.println("\nQuestion 4");
+
+    System.out.println("\nQuestion 5");
+
+    System.out.println("\nQuestion 6");
+
+    System.out.println("\nQuestion 7");
+
+    System.out.println("\nQuestion 8");
+
+    System.out.println("\nQuestion 9");
+
+    System.out.println("\nQuestion 10");
+
+  }
+
+  // Get a random number between 0 and 9 inclusive
+  // Helper function for question 10
+  public static int randomNumber() {
+      return new Random().nextInt(10); 
+  }
+}
